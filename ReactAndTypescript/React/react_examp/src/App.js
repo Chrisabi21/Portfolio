8fb8457bb64e959dbb8aec947841753f742fba28
Oluwaseun Abiodun <seunabi20@gmail.com>
2026-02-25 03:36:15 +0000

    update

---
+++ ReactAndTypescript/React/react_examp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>This is the Header</h1>
+    </header>
+  );
+}
+
+function App() {
+  return <Header />;
+}
+
+export default App;
